cb65534156f51f5cb61c3e4ea08af792bdb42c6a
nguye <nguyenhoangviet251103@gmail.com>
2024-11-25 10:05:24 +0700

    Add project files.

---
+++ Models/ViewModels/RevenueStatisticViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models.ViewModels
+{
+    public class RevenueStatisticViewModel
+    {
+        public DateTime Date { get; set; }
+        public decimal Benefit { get; set; }//lợi nhuân
+        public decimal Revenues { get; set; }// tổng giá bán
+    }
+}
